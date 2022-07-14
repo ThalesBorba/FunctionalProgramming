fba40b7445c697a9a8c4e7c412a8648f99c2d2db
ThalesBorba <thales_borba@hotmail.com>
2022-07-13 23:34:39 -0300

    Update

--- src/main/java/com/functional/programming/FunctionMain.java
+++ src/main/java/com/functional/programming/FunctionMain.java
@@ -1,4 +1,32 @@
 package com.functional.programming;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 public class FunctionMain {
+    public static void main(String[] args) {
+
+        System.out.println("PA de n + 2: ");
+        oneToTen.stream().map(addTwo).forEach(System.out::println);
+
+        System.out.println("\nPA de (n + 2) * 10: ");
+        oneToTen.stream().map(addTwo).map(multiplyByTen).forEach(System.out::println);
+
+        System.out.println("\n" + multiplyAndConvert.apply("O resultado de 4 * 10 é: ", 4));
+    }
+
+    static List<Integer> oneToTen = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+    static Function<Integer, Integer> addTwo = number -> number + 2;
+
+    static Function<Integer, Integer> multiplyByTen = number -> number * 10;
+
+    static BiFunction<String, Integer, String> multiplyAndConvert = (string, number) -> string + (number * 10);
+
+
+
+
+
 }
--- src/main/java/com/functional/programming/StreamMain.java
+++ src/main/java/com/functional/programming/StreamMain.java
@@ -4,6 +4,7 @@ import com.functional.programming.models.Student;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import static com.functional.programming.enums.Gender.FEMALE;
 import static com.functional.programming.enums.Gender.MALE;
@@ -15,13 +16,22 @@ public class StreamMain {
         //listFemaleStudents(students);
         listFemaleStudentsStream(students);
 
-        System.out.println("\nA nota dos aprovados são: ");
+        System.out.println("\nA nota dos aprovados, em ordem crescente de nota são: ");
         students
                 .stream()
                 .map(student -> student.getGrade())
                 .filter(grade -> grade >= 5.0)
                 .sorted()
                 .forEach(System.out::println);
+
+        System.out.println("\nAlunos que são mulheres e foram aprovados, ou que o nome começa com J: ");
+        students
+                .stream()
+                .filter(isFemale.and(isApproved).or(nameStartsWithJ))
+                .map(student -> student.getName())
+                .toList()
+                .forEach(System.out::println);
+
     }
 
     static List<Student> students = List.of(
@@ -29,8 +39,8 @@ public class StreamMain {
             new Student("Maria", FEMALE, 9.5),
             new Student("João", MALE, 5.8),
             new Student("Michelle", FEMALE, 7.5),
-            new Student("Bianca", FEMALE, 8.0),
-            new Student("Paulo", MALE, 4.0)
+            new Student("Bianca", FEMALE, 3.2),
+            new Student("Paulo", MALE, 6.0)
     );
 
     static void listFemaleStudents(List<Student> students) {
@@ -50,6 +60,10 @@ public class StreamMain {
         students.stream().filter(student -> FEMALE.equals(student.getGender())).map(student -> student.getName())
                 .forEach(System.out::println);
     }
+
+    static Predicate<Student> isFemale = student -> student.getGender().equals(FEMALE);
+    static Predicate<Student> isApproved = student -> student.getGrade() >= 5.0;
+    static Predicate<Student> nameStartsWithJ = student -> student.getName().startsWith("J");
 
 }
 
